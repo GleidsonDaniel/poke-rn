38b6da0a9a58fba3bd5a36ee6f8c9dd1b633c839
Daniel <gleidson10daniel@hotmail.com>
2021-05-08 11:02:13 -0300

    Add swr

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,17 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import Routes from './routes';
+import {SWRConfig} from 'swr';
+import {fetcher} from './core/helpers/swrFetcher';
 
 export default function index() {
-  return <Routes />;
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher,
+      }}>
+      <Routes />
+    </SWRConfig>
+  );
 }
