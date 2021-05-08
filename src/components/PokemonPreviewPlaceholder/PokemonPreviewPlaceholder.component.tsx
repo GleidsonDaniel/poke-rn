872a1e3981e44c0330265641a39a028c2000306e
Daniel <gleidson10daniel@hotmail.com>
2021-05-08 17:24:53 -0300

    Create PokemonPreviewPlaceholder.component

---
+++ src/components/PokemonPreviewPlaceholder/PokemonPreviewPlaceholder.component.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Dimensions} from 'react-native';
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
+
+export const PokemonPreviewPlaceholder = () => {
+  const {width} = Dimensions.get('screen');
+  return (
+    <SkeletonPlaceholder>
+      {[...Array(3).keys()].map(() => (
+        <SkeletonPlaceholder.Item
+          flexDirection="row"
+          alignItems="center"
+          padding={10}
+          justifyContent={'space-around'}>
+          <SkeletonPlaceholder.Item
+            width={width / 2.4}
+            height={150}
+            borderRadius={4}
+          />
+          <SkeletonPlaceholder.Item
+            width={width / 2.4}
+            height={150}
+            borderRadius={4}
+          />
+        </SkeletonPlaceholder.Item>
+      ))}
+    </SkeletonPlaceholder>
+  );
+};
