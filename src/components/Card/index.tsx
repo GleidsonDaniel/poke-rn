394542c3043044cb5e41154cb68b47128104b037
Daniel <gleidson10daniel@hotmail.com>
2021-05-08 10:29:20 -0300

    Create base card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, StyleSheet, Platform, ViewProps} from 'react-native';
+
+interface ICard extends ViewProps {
+  backgroundColor?: string;
+  elevation?: number;
+  cornerRadius?: number;
+  opacity?: number;
+  width?: number;
+}
+
+const Card: React.FC<ICard> = ({
+  children,
+  backgroundColor = '#ffffff',
+  elevation = 3,
+  cornerRadius = 5,
+  opacity = 0.5,
+  ...props
+}) => {
+  const cardStyle =
+    Platform.OS === 'ios'
+      ? StyleSheet.create({
+          container: {
+            shadowRadius: elevation,
+            shadowOpacity: opacity,
+            shadowOffset: {width: 0, height: elevation},
+            borderRadius: cornerRadius,
+            backgroundColor: backgroundColor,
+          },
+        })
+      : StyleSheet.create({
+          container: {
+            elevation: elevation,
+            borderRadius: cornerRadius,
+            backgroundColor: backgroundColor,
+          },
+        });
+
+  return <View style={[cardStyle.container, props.style]}>{children}</View>;
+};
+
+export default Card;
