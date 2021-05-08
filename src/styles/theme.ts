665e73ad592bb08cbcd0b1ab5891bc99b540ca98
Daniel <gleidson10daniel@hotmail.com>
2021-05-08 15:33:16 -0300

    add gray14/mediumGray

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,6 @@
 const colors = {
-  cyan: '#05bcfa',
-  mediumGray: '#979797',
-  salmone: '#FA5961',
-  brown: '#6c2e00',
+  gray14: '#363636',
+  mediumGray: '#5e5e5e',
 };
 
 export const theme = {
